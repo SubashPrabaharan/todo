5d8172a1a0ca9faa4562c70b589254788d877934
SubashPrabaharan <subashrm77@gmail.com>
2026-02-21 09:10:41 +0000

    feat:changes were made in task list and complete task

--- todo.js
+++ todo.js
@@ -1,12 +1,41 @@
 const addBtn =  document.getElementById('addTask');
 const taskList = document.getElementById('taskList');
+const completedTask = document.getElementById('completedTask');
 
 addBtn.addEventListener('click',function(){
+
+    const taskContainer  = document.createElement('div');
     const task = document.createElement('textarea');
     task.style.backgroundColor = color();
+    
+    const complete =  document.createElement('button'); 
+    complete.textContent = "Complete";
+    complete.addEventListener('click',function(){
+        taskContainer.remove();
+        completedTask.appendChild(taskContainer);
+        complete.remove();
+    })
 
-    taskList.appendChild(task);
+    
+    
+    taskContainer.appendChild(task);
+    taskContainer.appendChild(complete);
+    // taskList.appendChild(task);
+    taskList.prepend(taskContainer);
+
+   
 })
+// task.addEventListener('click',function(){
+//     const complete =  document.createElement('button');
+//     complete.textContent = "Complete";
+//     task.appendChild(complete);
+// })
+
+// task = document.createElement('textarea');
+// task.addEventListener('click',function(){
+//  const complete =  document.createElement('button');
+//  complete.textContent = "Complete";
+//  task.appendChild(taskList);
 
 const hex =  [0,1,2,3,4,5,6,7,8,9,`A`,`B`,`C`,`D`,`E`,`F`];
 
